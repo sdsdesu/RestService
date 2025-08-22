97fa09202de85d8afbae1c4aaf7e27b93dee5066
Sander De Sutter <sdsdesutter@live.be>
2025-08-22 12:02:48 +0200

    Integration tests

---
+++ RestIntegrationTest/SterrebeeldTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.Net.Http;
+using RestService.Controllers;
+using RestService.Services;
+using Moq;
+
+namespace RestIntegrationTest
+{
+    [TestClass]
+    public sealed class SterrebeeldTest
+    {
+        HttpClient _client = null!;
+        [TestInitialize]
+        public void Initialize()
+        {
+            var factory = new WebApplicationFactory<SterrebeeldControler>();
+            _client = factory.CreateDefaultClient();
+        }
+        [TestMethod]
+        public async Task VerwerkRamDatum()
+        {
+            var response = await _client.GetAsync("sterrebeelden/21-3");
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
---
+++ RestIntegrationTest/SterrebeeldTest2.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.Net.Http;
+using RestService.Controllers;
+using RestService.Services;
+using System.Threading.Tasks;
+using Moq;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace RestIntegrationTest
+{
+    [TestClass]
+    public sealed class SterrebeeldTest2
+    {
+        HttpClient client = null!;
+        Mock<IDatumLezerService> mockService = null!;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            mockService = new Mock<IDatumLezerService>();
+            var service = mockService.Object;
+            var factory = new WebApplicationFactory<SterrebeeldControler>();
+            client = factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    services.AddScoped<IDatumLezerService>(_ => service);
+                });
+            }).CreateClient();
+        }
+        [TestMethod]
+        public async Task VerwerkRamDatum()
+        {
+            mockService.Setup(Mock => Mock.sterrebeeldNaam(21, 3)).ReturnsAsync("Ram");
+            var response = await client.GetAsync("sterrebeelden/21-3");
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            mockService.Verify(mock => mock.sterrebeeldNaam(21, 3));
+
+        }
+    }
+}
